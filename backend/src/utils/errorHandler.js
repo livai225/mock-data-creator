bde54d07afd832a3d3099a6631fe08e6ccf1e5bb
hexpertise <hexpertise@vmi2967615.contaboserver.net>
2026-01-01 15:10:27 +0100

    Fix: Correction erreur syntaxe ClientDashboard (div en trop) + amélioration messages erreur auth

---
+++ backend/src/utils/errorHandler.js
@@ -0,0 +1,14 @@
+/**
+ * Utilitaire de gestion d'erreurs
+ */
+
+export class AppError extends Error {
+  constructor(message, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default AppError;
